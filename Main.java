a41712873587464a87a707c5c0c9651b338f433d
idembele70 <idembele70@gmail.com>
2021-03-14 22:21:08 +0100

    sum pas fini

--- Main.java
+++ Main.java
@@ -1,11 +1,16 @@
+import java.sql.Array;
+import java.util.stream.Stream;
 
 /**
  * Main
  */
 public class Main {
     public static void main(String[] args) {
-     System.out.println("Salut");   
+        
+System.out.println(sum(new int[]{-1,2,3}));
     }
 
-    
+    public static int sum(int[] arr) {
+    }
+   
 }
