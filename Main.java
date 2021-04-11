a68a3fa664ff7b2d85969462347d109591bcd9b8
idembele70 <idembele70@gmail.com>
2021-04-11 18:52:37 +0200

    return Negative

--- Main.java
+++ Main.java
@@ -1,21 +1,12 @@
-import java.sql.Array;
-import java.util.stream.Stream;
-
 /**
  * Main
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println(sum(new int[] { 1, 2, 3, 4, 5 }));
+        System.out.println(makeNegative(-12));
     }
 
-    public static int sum(int[] arr) {
-        int res = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > 0) {
-                res += arr[i];
-            }
-        }
-        return res;
-    }
+    public static int makeNegative(final int x) {
+        return Math.negateExact(Math.abs(x));
+}
 }
