0242a60c4f0e11e51cd237e3b7437f412c0cc825
idembele70 <idembele70@gmail.com>
2021-04-25 15:05:05 +0200

    greet

--- Main.java
+++ Main.java
@@ -1,14 +1,16 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(repeatStr(4, "a"));
+        String namer = "joh";
+        System.out.println(String.format(" %s!",namer));
+        System.out.println(greet("Johnny"));
     }
 
-    public static String repeatStr(final int repeat, final String string) {
-        String stringRepeated ="";
-        for (int i = 0; i < repeat; i++) {
-            stringRepeated+=string;
+    public static String greet(String name) {
+        if (name.equals("Johnny")) {
+            return "Hello, my love!";
+        } else {
+            return "Hello, "+name+"!";
         }
-        return stringRepeated;
     }
 }
